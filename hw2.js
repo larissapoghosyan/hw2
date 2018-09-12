5bbdbdf8f0b9f173d063ad812dc95035ca128a0d
Larisa Poghosyan <larissa.poghosyan@gmail.com>
2018-09-13 01:50:00 +0400

    dsd

---
+++ hw2.js
@@ -0,0 +1,12 @@
+const x = 4;
+const y = (x+7)*x;
+const z = (b-5)/2;
+
+
+const oneNumber = 6;
+const oneString = "Lareesa";
+if( y > 7) {
+	console.log("Ninjas rule");
+} else {
+	console.log(oneString);
+}
